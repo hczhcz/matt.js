39eaf56e3643c1002f4783dcdc450f5daa6e797b
hcz <h.cz@qq.com>
2016-03-19 19:11:32 -0400

    implement UnixUser

--- interface.ts
+++ interface.ts
@@ -9,7 +9,10 @@ export interface Context {
 
 export interface User {
     name(c_: Context, cb_: ValFunc<string>): void;
-    distance(user: User): number;
+    distance(c_: Context, user: User, cb_: ValFunc<number>): void;
+
+    // unix
+    group?(c_: Context, cb_: ValFunc<string>): void;
 };
 
 export enum ModeActions {read, write, exec};
@@ -24,21 +27,21 @@ export interface File {
     chown  (c_: Context, owner: User, cb_: VoidFunc, fl_: ErrFunc): void;
 
     // dir
-    readdir(c_: Context, cb_: ValFunc<string[]>, fl_: ErrFunc): void;
-    open   (c_: Context, name: string, cb_: ValFunc<File>, fl_: ErrFunc): void;
-    link   (c_: Context, name: string, file: File, cb_: VoidFunc, fl_: ErrFunc): void;
-    unlink (c_: Context, name: string, cb_: ValFunc<File>, fl_: ErrFunc): void;
-    swap   (c_: Context, name: string, file: File, cb_: ValFunc<File>, fl_: ErrFunc): void;
+    readdir?(c_: Context, cb_: ValFunc<string[]>, fl_: ErrFunc): void;
+    open?   (c_: Context, name: string, cb_: ValFunc<File>, fl_: ErrFunc): void;
+    link?   (c_: Context, name: string, file: File, cb_: VoidFunc, fl_: ErrFunc): void;
+    unlink? (c_: Context, name: string, cb_: ValFunc<File>, fl_: ErrFunc): void;
+    swap?   (c_: Context, name: string, file: File, cb_: ValFunc<File>, fl_: ErrFunc): void;
 
     // link
-    readlink (c_: Context, cb_: ValFunc<File>, fl_: ErrFunc): void;
-    writelink(c_: Context, file: File, cb_: VoidFunc, fl_: ErrFunc): void;
+    readlink? (c_: Context, cb_: ValFunc<File>, fl_: ErrFunc): void;
+    writelink?(c_: Context, file: File, cb_: VoidFunc, fl_: ErrFunc): void;
 
     // file // TODO: stream io?
-    // read (c_: Context, , cb_: VoidFunc, fl_: ErrFunc): void;
-    // write(c_: Context, , cb_: VoidFunc, fl_: ErrFunc): void;
+    // read? (c_: Context, , cb_: VoidFunc, fl_: ErrFunc): void;
+    // write?(c_: Context, , cb_: VoidFunc, fl_: ErrFunc): void;
 
     // json
-    readjson (c_: Context, cb_: ValFunc<JSON>, fl_: ErrFunc): void;
-    writejson(c_: Context, json: JSON, cb_: VoidFunc, fl_: ErrFunc): void;
+    readjson? (c_: Context, cb_: ValFunc<JSON>, fl_: ErrFunc): void;
+    writejson?(c_: Context, json: JSON, cb_: VoidFunc, fl_: ErrFunc): void;
 };
--- user.ts
+++ user.ts
@@ -1,3 +1,36 @@
 'use strict';
 
+import {ValFunc} from './util';
 import {Context, User} from './interface';
+
+export class UnixUser implements User {
+    constructor(private _group: string, private _name: string) {
+        //
+    }
+
+    group(context: Context, callback: ValFunc<string>) {
+        callback(this._group);
+    }
+
+    name(context: Context, callback: ValFunc<string>) {
+        callback(this._name);
+    }
+
+    distance(context: Context, user: User, callback: ValFunc<number>) {
+        user.name(context, (name: string) => {
+            if (name === this._name) {
+                callback(0);
+            } else if (user.group) {
+                user.group(context, (group: string) => {
+                    if (group === this._group) {
+                        callback(1);
+                    } else {
+                        callback(Infinity);
+                    }
+                })
+            } else {
+                callback(Infinity);
+            }
+        });
+    }
+};
