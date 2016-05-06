df5de1b4c2bdfc69c01cbc017a7f7a1b5df894b2
hcz <h.cz@qq.com>
2016-05-06 15:55:27 +0800

    working

--- test.ts
+++ test.ts
@@ -1,71 +1,60 @@
 'use strict';
 
-import {ErrFunc, VoidFunc, ValFunc} from './util';
-import {Context, User, ModeActions, Mode, Node} from './interface';
+import {SimpleError, ErrFunc, VoidFunc, ValFunc} from './util';
+import {User, Node} from './interface';
 import {UnixUser, UnixSuperUser} from './user'
 import {UnixMode} from './mode';
-import {DirNode, JsonObjNode, FuncObjNode} from './node';
+import {DirNode, FuncObjNode} from './node';
 
-const root: Node = new DirNode(
-    new UnixMode(7, 5, 5),
-    new UnixSuperUser(),
-    [
-        ['auth', new DirNode(
-            new UnixMode(7, 5, 5),
-            new UnixSuperUser(),
-            [
-                ['root', new FuncObjNode(
-                    new UnixMode(4, 4, 4),
-                    new UnixSuperUser(),
-                    (callback: ValFunc<any>, fail: ErrFunc): void => {
-                        callback(new UnixSuperUser()); // TODO: ask password?
-                    },
-                    (obj: any, callback: VoidFunc, fail: ErrFunc): void => {
-                        fail();
-                    }
-                )],
-                ['test', new FuncObjNode(
-                    new UnixMode(4, 4, 4),
-                    new UnixUser('test', 'test'),
-                    (callback: ValFunc<any>, fail: ErrFunc): void => {
-                        callback(new UnixUser('test', 'test')); // TODO: ask password?
-                    },
-                    (obj: any, callback: VoidFunc, fail: ErrFunc): void => {
-                        fail();
-                    }
-                )],
-            ]
-        )],
-        ['bin', new DirNode(
-            new UnixMode(7, 5, 5),
-            new UnixSuperUser(),
-            []
-        )],
-        ['dev', new DirNode(
-            new UnixMode(7, 5, 5),
-            new UnixSuperUser(),
-            []
-        )],
-        ['home', new DirNode(
-            new UnixMode(7, 5, 5),
-            new UnixSuperUser(),
-            [
-                ['root', new DirNode(
-                    new UnixMode(7, 0, 0),
-                    new UnixSuperUser(),
-                    []
-                )],
-                ['test', new DirNode(
-                    new UnixMode(7, 0, 0),
-                    new UnixUser('test', 'test'),
-                    []
-                )],
-            ]
-        )],
-        ['proc', new DirNode(
-            new UnixMode(5, 5, 5),
-            new UnixSuperUser(),
-            []
-        )],
-    ]
-);
+function makeRootDir(list: [string, Node][]): Node {
+    return new DirNode(new UnixMode(7, 5, 5), new UnixSuperUser(), list);
+}
+
+function makeSysDir(list: [string, Node][]): Node {
+    return new DirNode(new UnixMode(5, 5, 5), new UnixSuperUser(), list);
+}
+
+function makeAuth(user: User, password: string): Node {
+    return new FuncObjNode(
+        new UnixMode(4, 4, 4),
+        user,
+        (callback: ValFunc<any>, fail: ErrFunc): void => {
+            callback(user); // TODO: ask password?
+        },
+        (obj: any, callback: VoidFunc, fail: ErrFunc): void => {
+            fail(new SimpleError('not writable'));
+        }
+    );
+}
+
+function makeUserAuth(group: string, name: string, password: string): Node {
+    return makeAuth(new UnixUser(group, name), password);
+}
+
+function makeSuperUserAuth(password: string): Node {
+    return makeAuth(new UnixSuperUser(), password);
+}
+
+function makeUserHome(group: string, name: string): Node {
+    return new DirNode(new UnixMode(7, 5, 5), new UnixUser(group, name), []);
+}
+
+function makeSuperUserHome(): Node {
+    return new DirNode(new UnixMode(7, 0, 0), new UnixSuperUser(), []);
+}
+
+function makeRoot(password: string): Node {
+    return makeRootDir([
+        ['auth', makeRootDir([
+            ['root', makeSuperUserAuth(password)],
+        ])],
+        ['bin', makeRootDir([])],
+        ['dev', makeSysDir([])],
+        ['home', makeRootDir([
+            ['root', makeSuperUserHome()],
+        ])],
+        ['proc', makeSysDir([])],
+    ]);
+}
+
+makeRoot('test');
