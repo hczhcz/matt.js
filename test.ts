e6ab65b1bcfbae109ce4981684f6940ad06ae3f3
hcz <h.cz@qq.com>
2016-05-06 15:27:55 +0800

    working

--- test.ts
+++ test.ts
@@ -4,17 +4,38 @@ import {ErrFunc, VoidFunc, ValFunc} from './util';
 import {Context, User, ModeActions, Mode, Node} from './interface';
 import {UnixUser, UnixSuperUser} from './user'
 import {UnixMode} from './mode';
-import {DirNode, FreeObjNode, JsonObjNode} from './node';
+import {DirNode, JsonObjNode, FuncObjNode} from './node';
 
 const root: Node = new DirNode(
     new UnixMode(7, 5, 5),
     new UnixSuperUser(),
     [
-        // ['auth', new DirNode(
-        //     new UnixMode(7, 5, 5),
-        //     new UnixSuperUser(),
-        //     []
-        // )],
+        ['auth', new DirNode(
+            new UnixMode(7, 5, 5),
+            new UnixSuperUser(),
+            [
+                ['root', new FuncObjNode(
+                    new UnixMode(4, 4, 4),
+                    new UnixSuperUser(),
+                    (callback: ValFunc<any>, fail: ErrFunc): void => {
+                        callback(new UnixSuperUser()); // TODO: ask password?
+                    },
+                    (obj: any, callback: VoidFunc, fail: ErrFunc): void => {
+                        fail();
+                    }
+                )],
+                ['test', new FuncObjNode(
+                    new UnixMode(4, 4, 4),
+                    new UnixUser('test', 'test'),
+                    (callback: ValFunc<any>, fail: ErrFunc): void => {
+                        callback(new UnixUser('test', 'test')); // TODO: ask password?
+                    },
+                    (obj: any, callback: VoidFunc, fail: ErrFunc): void => {
+                        fail();
+                    }
+                )],
+            ]
+        )],
         ['bin', new DirNode(
             new UnixMode(7, 5, 5),
             new UnixSuperUser(),
@@ -28,7 +49,18 @@ const root: Node = new DirNode(
         ['home', new DirNode(
             new UnixMode(7, 5, 5),
             new UnixSuperUser(),
-            []
+            [
+                ['root', new DirNode(
+                    new UnixMode(7, 0, 0),
+                    new UnixSuperUser(),
+                    []
+                )],
+                ['test', new DirNode(
+                    new UnixMode(7, 0, 0),
+                    new UnixUser('test', 'test'),
+                    []
+                )],
+            ]
         )],
         ['proc', new DirNode(
             new UnixMode(5, 5, 5),
