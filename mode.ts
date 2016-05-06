61b1dad58ce1f90b70253425c3493daec409f532
hcz <h.cz@qq.com>
2016-05-06 12:31:58 +0800

    add public access trial in UnixMode

--- mode.ts
+++ mode.ts
@@ -16,25 +16,37 @@ export class UnixMode implements Mode {
         callback(String(this._user) + String(this._group) + String(this._all));
     }
 
+    private _check(
+        distance: number, action: ModeActions,
+        accept: VoidFunc, denied: VoidFunc
+    ): void {
+        const mode: number = distance === 0 ? this._user
+                           : distance === 1 ? this._group
+                           : this._all;
+        const mask: number = action === ModeActions.read ? 4
+                           : action === ModeActions.write ? 2
+                           : action === ModeActions.exec ? 1
+                           : action === ModeActions.attr && distance === 0 ? 0 // always ok
+                           : 255; // always deny
+
+        if ((mode & mask) === mask) {
+            accept();
+        } else {
+            denied();
+        }
+    }
+
     check(
         context: Context, owner: User, action: ModeActions,
         callback: VoidFunc, fail: ErrFunc
     ): void {
-        owner.distance(context, (distance: number): void => {
-            const mode = distance === 0 ? this._user
-                       : distance === 1 ? this._group
-                       : this._all;
-            const mask = action === ModeActions.read ? 4
-                       : action === ModeActions.write ? 2
-                       : action === ModeActions.exec ? 1
-                       : action === ModeActions.attr && distance === 0 ? 0 // always ok
-                       : 255; // always deny
-
-            if ((mode & mask) === mask) {
-                callback();
-            } else {
-                fail(new SimpleError('access denied'));
-            }
-        }, fail);
+        this._check(Infinity, action, callback, (): void => {
+            // public access denied, apply user's permission
+            owner.distance(context, (distance: number): void => {
+                this._check(distance, action, callback, (): void => {
+                    fail(new SimpleError('access denied'));
+                });
+            }, fail);
+        });
     }
 };
