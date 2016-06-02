d3a7007b3e339abe7e3414474d49fac9b7967e7d
hcz <h.cz@qq.com>
2016-06-02 17:29:14 +0800

    add errMethod

--- util.ts
+++ util.ts
@@ -13,3 +13,7 @@ export interface VoidFunc {
 export interface ValFunc<T> {
     (value: T): void;
 };
+
+export function errMethod(...args: any[]): void {
+    <ErrFunc>(args[args.length - 1])(new SimpleError('not supported'));
+}
